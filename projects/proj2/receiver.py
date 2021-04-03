4401cb63c022e5dd1c223e9cc88e9c242347d354
C-Lizzo14 <cxl5858@gmail.com>
2021-04-02 23:57:47 -0400

    task 1 last minute fix

--- projects/proj2/receiver.py
+++ projects/proj2/receiver.py
@@ -1,28 +1,40 @@
-# ----- receiver.py -----
+#-------receiver.py---------#
 
 #!/usr/bin/env python
 
 from socket import *
 import sys
 import select
+#import time
+#import json
 
-host="0.0.0.0"
-port = 9999
+host = "0.0.0.0"
+port = 9999  # int(sys.argv[1])
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind((host,port))
 
+# Change package size if the thing breaks (Goal:1400 bytes )
+buf = 1024
 addr = (host,port)
-buf=1024
+#addr = (port)
 
-f = open("received.txt",'wb')
+#f = open("received.txt",'wb')
 
+#s.settimeout(2)
+
+#total_data = 0
 data,addr = s.recvfrom(buf)
+# f = open("received.txt",'wb')
+
 try:
-    while(data):
-        f.write(data)
-        s.settimeout(2)
-        data,addr = s.recvfrom(buf)
+	while(data):
+
+		sys.stdout.write(data.decode())
+		s.settimeout(2)
+		data,addr = s.recvfrom(buf)
+
+
 except timeout:
-    f.close()
-    s.close()
-    print("File Downloaded")
+       #exit(2)
+	sys.stderr.write("File received, exiting. ")
+	s.close()
--- projects/proj2/sender.py
+++ projects/proj2/sender.py
@@ -1,20 +1,46 @@
 # ----- sender.py ------
+# ----- sender.py ------
 
 #!/usr/bin/env python
 from socket import *
 import sys
+import select
+import time
+
 
 s = socket(AF_INET,SOCK_DGRAM)
 host =sys.argv[1]
-port = 9999
+
+port = 9999 #int(sys.argv[])
+
+#probably gonna have to change
 buf =1024
 addr = (host,port)
+#s.bind((host,port))
+data = sys.stdin.read(buf).encode()
+# f=open("a.txt","rb")
 
-f=open("a.txt","rb")
-data = f.read(buf)
+
+data,addr = s.recvfrom(buf)
+time_start = time()
+kb_total = 0
+timeSec = 0
+
 while (data):
-    if(s.sendto(data,addr)):
-        print("sending ...")
-        data = f.read(buf)
+
+        if(s.sendto(data,addr)):
+
+                print("sending ...")
+                data = sys.stdin.read(buf).encode()
+                time_curr = time.time()
+                time_total = time_curr - time_start
+
+                kb_total = kb_total + 1
 s.close()
-f.close()
+
+time_total = float("%0.3f" % (time_total))
+bytes = kb_total * 1024
+kb_seconds = kb_total/time_total
+kb_seconds = float("%0.3f" % (kb_secconds))
+print("Sent" + str(bytes) + " bytes in " + str(time_total) + " seconds: " + str(kb_seconds) + " kb/s")
+
